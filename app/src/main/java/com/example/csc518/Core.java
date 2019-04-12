b60c6b3ee2cb4ea72e92e59b473e17123987c664
Madie Martens <madiemm97@gmail.com>
2019-04-12 18:05:30 -0500

    Database update

---
+++ app/src/main/java/com/example/csc518/Core.java
@@ -0,0 +1,11 @@
+package com.example.csc518;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Core
+{
+    public static FirebaseDatabase database;
+    public static DatabaseReference mealsRef;
+    //public static DatabaseReference loyaltyProgramRef;
+}
--- app/src/main/java/com/example/csc518/MealsPage.java
+++ app/src/main/java/com/example/csc518/MealsPage.java
@@ -2,12 +2,49 @@ package com.example.csc518;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MealsPage extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import static com.example.csc518.Core.database;
+
+public class MealsPage extends AppCompatActivity
+{
+    private EditText numberOfMealsET;
+    private Button submitButton;
+    private MealsPage myself;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("***Made it");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meals_page);
+
+        Core.database = FirebaseDatabase.getInstance();
+        //Core.mealsRef = FirebaseDatabase.getInstance().getReference();
+        Core.mealsRef = database.getReference("meals");
+        this.myself = this;
+
+        //myRef.setValue("Hello, World!");
+
+        this.numberOfMealsET = (EditText)findViewById(R.id.numberOfMealsET);
+        this.submitButton = (Button)findViewById(R.id.submitButton);
+
+//        submitButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Core.mealsRef.setValue(numberOfMealsET.getText().toString());
+//            }
+//        });
+    }
+
+    public void onSubmitButtonPressed(View v)
+    {
+        Core.mealsRef.setValue(numberOfMealsET.getText().toString());
+        System.out.println("*** reached button");
+        //Core.mealsRef.addListenerForSingleValueEvent();
     }
 }
