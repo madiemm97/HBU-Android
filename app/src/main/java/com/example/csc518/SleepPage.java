1c24f0889d983d003d9e0bc013b7e8071103ea49
gracekoester1 <grace.koester@cuw.edu>
2019-05-01 11:46:16 -0500

    Update SleepPage
    Hours slept, need to change values

--- app/src/main/java/com/example/csc518/SleepPage.java
+++ app/src/main/java/com/example/csc518/SleepPage.java
@@ -1,13 +1,117 @@
 package com.example.csc518;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SleepPage extends AppCompatActivity {
+
+    private Button sleep1;
+    private Button sleep2;
+    private Button sleep3;
+    private Button sleep4;
+    private Button sleep5;
+    private Button sleep6;
+    private Button sleep7;
+    private Button sleep8;
+    private Button sleep9ormore;
+    private int totalSleep;
+    public Button submitButton;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sleep_page);
+
+        this.sleep1 = (Button)(findViewById(R.id.sleep1));
+        this.sleep2 = (Button)(findViewById(R.id.sleep2));
+        this.sleep3 = (Button)(findViewById(R.id.sleep3));
+        this.sleep4 = (Button)(findViewById(R.id.sleep4));
+        this.sleep5 = (Button)(findViewById(R.id.sleep5));
+        this.sleep6 = (Button)(findViewById(R.id.sleep6));
+        this.sleep7 = (Button)(findViewById(R.id.sleep7));
+        this.sleep8 = (Button)(findViewById(R.id.sleep8));
+        this.sleep9ormore = (Button)(findViewById(R.id.sleep9ormore));
+        this.submitButton = (Button)(findViewById(R.id.submitButton));
+
+
     }
+
+    public void onSleep1ButtonPressed(View v)
+    {
+        sleep1.setBackgroundColor(Color.GRAY);
+
+        totalSleep = 1;
+    }
+
+    public void onSleep2ButtonPressed(View v)
+    {
+        sleep2.setBackgroundColor(Color.GRAY);
+
+        totalSleep = 2;
+    }
+
+    public void onSleep3ButtonPressed(View v)
+    {
+        sleep3.setBackgroundColor(Color.GRAY);
+
+        totalSleep = 3;
+    }
+
+    public void onSleep4ButtonPressed(View v)
+    {
+        sleep4.setBackgroundColor(Color.GRAY);
+
+        totalSleep = 4;
+    }
+
+    public void onSleep5ButtonPressed(View v)
+    {
+        sleep5.setBackgroundColor(Color.GRAY);
+
+        totalSleep = 5;
+    }
+
+    public void onSleep6ButtonPressed(View v)
+    {
+        sleep6.setBackgroundColor(Color.GRAY);
+
+        totalSleep = 6;
+    }
+
+    public void onSleep7ButtonPressed(View v)
+    {
+        sleep7.setBackgroundColor(Color.GRAY);
+
+        totalSleep = 7;
+    }
+
+    public void onSleep8ButtonPressed(View v)
+    {
+        sleep8.setBackgroundColor(Color.GRAY);
+
+        totalSleep = 8;
+    }
+
+    public void onSleep9ButtonPressed(View v)
+    {
+        sleep9ormore.setBackgroundColor(Color.GRAY);
+
+        totalSleep = 9;
+    }
+
+    public void onSubmitButtonPressed(View v)
+    {
+        Intent i = new Intent(this, SleepPage.class);
+        this.startActivity(i);
+        System.out.println(totalSleep);
+    }
+
+
 }
